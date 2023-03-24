67342b8eddd93a516abc9e00f95991c6f956afbf
Alexmateos2 <127136380+Alexmateos2@users.noreply.github.com>
2023-03-24 13:15:50 +0100

    Delete Clase 23-03 directory

--- Clase 23-03/autos.js
+++
@@ -1,6 +0,0 @@
-let carros = ["Nissan March", "Chevrolet Aveo","Volkswagen Taos", "KIA Rio Sedan"];
-alert(carros);
-carros.pop();
-carros.push("Hyundai Creta");
-carros[0]="Nissan Versa";
-alert(carros);
