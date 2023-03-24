bcc9f6d607d200b470efbd1d78605e68f2d919c5
Alejandro FG <alexjandro1402_@hotmail.com>
2023-03-24 16:03:10 +0100

    Metí Día 14

---
+++ Día 14/extra14.js
@@ -0,0 +1,7 @@
+let carros = ["Nissan March", "Chevrolet Aveo", "Volkswagen Taos", "KIA Rio Sedan"];
+carros.pop();
+carros.push("Hyundai Creta")
+carros.shift()
+carros.unshift("Nissan Versa")
+console.log(carros);
+
