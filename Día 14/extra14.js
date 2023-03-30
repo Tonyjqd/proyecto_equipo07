9a235b9df7849eaadaf98e621dfa42c9f6b6a122
Alejandro FG <alexjandro1402_@hotmail.com>
2023-03-30 19:38:51 +0200

    .

--- Día 14/extra14.js
+++
@@ -1,7 +0,0 @@
-let carros = ["Nissan March", "Chevrolet Aveo", "Volkswagen Taos", "KIA Rio Sedan"];
-carros.pop();
-carros.push("Hyundai Creta")
-carros.shift()
-carros.unshift("Nissan Versa")
-console.log(carros);
-
