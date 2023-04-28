278a382932577cb8eb71f8a78c9a0552de1275dc
alexmateos <alexmateosxd@gmail.com>
2023-04-28 12:32:13 +0200

    He divido la carpeta en backend - frontend , en backend estan los modulos express mysql2 y cors ignorados con .gitignore, tiene un archivo server con las acciones necesarias, he hecho un login funcional una comprobacion de que en main no se pueda entrar sin logearse y te redireccione, tambien en perfil se ajuste al usuario logeado mostrando todos sus datos.Por ultimo he movido mi perfil a amigos y solucionado fallos menores

--- Página Web/boton.js
+++
@@ -1,5 +0,0 @@
-const body = document.querySelector('body');
-const toggleMode = document.getElementById('toggle-dark-mode');     
-toggleMode.addEventListener('change', function() {
-body.classList.toggle('dark-mode');
-      });
